d57c517141c68c7ae04070ce5e233baaf3b60bae
Stefano D Minervini <minervini@mac.com>
2019-10-18 08:50:29 -0500

    project build

---
+++ BestBuyMVC/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using BestBuyMVC.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BestBuyMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ProductRepository repo = new ProductRepository();
+            List<Product> product = repo.GetAllProducts();
+
+            return View(product);
+        }
+    }
+}
---
+++ BestBuyMVC/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using BestBuyMVC.Models;
+using MySql.Data.MySqlClient;
+
+namespace BestBuyMVC
+{
+    public class ProductRepository
+    {
+        public List<Product> GetAllProducts()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT ProductID, Name, Price FROM products;";
+
+            using (conn)
+            {
+                conn.Open();
+                //cmd.ExecuteNonQuery();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                List<Product> allProducts = new List<Product>();
+                while (reader.Read())
+                {
+                    Product currentProduct = new Product();
+                    currentProduct.ProductID = reader.GetInt32("ProductID");
+                    currentProduct.Name = reader.GetString("Name");
+                    currentProduct.Price = reader.GetDecimal("Price");
+
+                    allProducts.Add(currentProduct);
+                }
+                return allProducts;
+            }
+        }
+    }
+}
